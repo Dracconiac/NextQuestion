a67a5ecb09e62f63a3ae2b6940cf38e5077fe1df
Dracconiac <martin.cernak.se@gmail.com>
2025-08-01 20:38:17 +0200

    Quiz.cs
    added Quiz.cs script that handles answer evaluation and changes button sprites and some texts based on the answer

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Quiz Questino", fileName = "New Question")]
@@ -7,6 +8,26 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
+    public string GetQuestion()
+    {
+        return question;
+    }
 
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        if (index < 0 || index >= answers.Length)
+        {
+            Debug.LogError("Index out of bounds for answers array");
+            return null;
+        }
+        return answers[index];
+    }
 }
---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+    void Start()
+    {
+        DisplayQuestion();
+        SetButtonState(true); // Enables buttons at the start and resets their sprites
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+
+        SetButtonState(false); // Disables all buttons after an answer is selected
+
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            questionText.text = "Wrong, the correct answer is:\n" + question.GetAnswer(correctAnswerIndex);
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+
+    }
+
+    void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite; // Reset to default sprite
+        }
+    }
+    
+}
